818051bbbd719fd40bbbdc9d59e48a76978d2dfa
Alishba964 <alishba.khan191206@gmail.com>
2026-07-18 20:08:31 +0500

    Console Based Recipe Search

--- first.py
+++ first.py
@@ -1,4 +1,3 @@
-# print("hello world")
 import requests
 import re
 from dotenv import load_dotenv
@@ -10,33 +9,98 @@ name=input("Enter name of food or main ingredients: ")
 url=f"https://api.spoonacular.com/recipes/complexSearch?query={name}&maxFat=25&apiKey={API_KEY}"
 response=requests.get(url)
 data=response.json()
-print(response.status_code)
 
+if (response.status_code != 200):
+    print("API ERROR")
+    print(response.text)
+    exit()
 
-print(data["results"][0]["title"])
+if not data["results"]:
+    print("No Recipes Found")
+    exit()
 
+for index , recipe in enumerate(data["results"]):
+    print(f"{index+1}. {recipe['title']}")
 
-id=data["results"][0]["id"]
-details_url=f"https://api.spoonacular.com/recipes/{id}/information?apiKey={API_KEY}"
+print("--------------------------------------")
+dish = int(input("Enter a Dish Number: "))
+if dish < 1 or dish > len(data["results"]):
+    print("Invalid recipe number.")
+    exit()
+selected_recipe = data["results"][dish - 1]
+recipe_id = selected_recipe["id"]
+
+# id=data["results"][0]["id"]
+details_url=f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={API_KEY}"
 details_response=requests.get(details_url)
+if details_response.status_code != 200:
+    print("Failed to fetch recipe details.")
+    print(details_response.text)
+    exit()
 details=details_response.json()
 
-print("READY IN MINUTES:", details["readyInMinutes"])
-print("SERVINGS: ", details["servings"])
-print("INGREDIENTS: ")
-for i in details["extendedIngredients"]:
-    print(i["originalName"]," | ",i["amount"], i["unit"])
+print("-----------------------------------------")
+print("                DETAILS                  ")
+print("-----------------------------------------")
+print("READY IN MINUTES:", details.get("readyInMinutes"),"Not Available")
+print("SERVINGS: ", details.get("servings","Not Available"))
+print("-----------------------------------------")
+print("               INGREDIENTS               ")
+print("-----------------------------------------")
 
-desc = re.sub(r"<.*?>", "",details["summary"] )
-print("DESCRIPTION: ",desc )
-print("Dish Type: ", details["dishTypes"])
-inst= re.sub(r"<.*?>", "",details["instructions"] )
-print("INSTRUCTIONS ", inst)
+for index ,i in enumerate(details["extendedIngredients"]):
+    print(index+1,". ",i.get("originalName","Unknown")," ( ",i.get("amount",""), i.get("unit","")," ) ")
+
+print("-----------------------------------------")
+print("             DESCRIPTION                 ")
+print("-----------------------------------------")
+summary = details.get("summary")
+if summary:
+    summary = re.sub(r"<.*?>", "", summary)
+else:
+    summary="No Description Available!"
+
+print("Description : ",summary)
+
+print("-----------------------------------------")
+print("              INSTRUCTIONS               ")
+print("-----------------------------------------")
+
+instructions = details.get("instructions")
+
+if instructions:
+    instructions = re.sub(r"<.*?>", "", instructions)
+    print(instructions)
+else:
+    print("Instructions not available.")
+
+print("-----------------------------------------")
+dish_types = details.get("dishTypes", [])
+print("Dish Type.......")
+if dish_types:
+    print("\n".join(dish_types))
+else:
+    print("Dish Type: Not Available")
 
 
+print("Health Score : ", details.get("healthScore","Not Available"))
+
+print("Source Website:", details.get("sourceUrl", "Not Available"))
+print("Image:", details.get("image", "Not Available"))
+
+cuisines = details.get("cuisines", [])
+print("Cuisine ..........")
+if cuisines:
+    print("\n".join(cuisines))
+else:
+    print("Cuisine: Not Available")
 
 
-# print("Recipe Name : ",data["results"][0]["title"])
-# print("NUTRIENTS")
-# print("NUTRIENT NAME : ",data["results"][0]["nutrition"]["nutrients"][0]["name"])
-# print("NUTRIENT AMOUNT : ",data["results"][0]["nutrition"]["nutrients"][0]["amount"],data["results"][0]["nutrition"]["nutrients"][0]["unit"])
+print("-----------------------------------------")
+print("            DIET INFORMATION             ")
+print("-----------------------------------------")
+print("Vegetarian :", "Yes" if details.get("vegetarian") else "No")
+print("Vegan      :", "Yes" if details.get("vegan") else "No")
+print("Gluten Free:", "Yes" if details.get("glutenFree") else "No")
+print("Dairy Free :", "Yes" if details.get("dairyFree") else "No")
+
